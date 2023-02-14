92fe077ca22ce2f202805e9f16065f64d5fe83db
MyFreeR <my.free@mail.ru>
2023-02-14 17:15:53 +0300

    3

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ from urllib3 import Retry
 
 class MainWindow(QMainWindow):
     g_map: QLabel
+    press_delta = 0.1
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi('main_window.ui', self)
-        self.press_delta = 0.00001
 
         self.map_zoom = 5
         self.map_ll = [37.977751, 55.757718]
@@ -25,17 +25,27 @@ class MainWindow(QMainWindow):
         self.refresh_map()
 
     def keyPressEvent(self, event):
-        if event.key() == Qt.Key_PageUp and self.map_zoom < 17:
+        key = event.key()
+        if key == Qt.Key_PageUp and self.map_zoom < 17:
             self.map_zoom += 1
-        if event.key() == Qt.Key_PageDown and self.map_zoom > 0:
+        if key == Qt.Key_PageDown and self.map_zoom > 0:
             self.map_zoom -= 1
+        if key == Qt.Key_Left:
+            self.map_ll[0] -= self.press_delta
+        if key == Qt.Key_Right:
+            self.map_ll[0] += self.press_delta
+        if key == Qt.Key_Up:
+            self.map_ll[1] += self.press_delta
+        if key == Qt.Key_Down:
+            self.map_ll[1] -= self.press_delta
+
         self.refresh_map()
 
     def refresh_map(self):
         map_params = {
-            "ll": ','.join(map(str, self.map_ll)),
+            "ll": f'{self.map_ll[0]},{self.map_ll[1]}',
             "l": self.map_l,
-            'z': self.map_zoom
+            'z': self.map_zoom,
         }
         session = requests.Session()
         retry = Retry(total=10, connect=5, backoff_factor=0.5)
@@ -49,6 +59,7 @@ class MainWindow(QMainWindow):
 
         pixmap = QPixmap()
         pixmap.load('tmp.png')
+
         self.g_map.setPixmap(pixmap)
 
 
